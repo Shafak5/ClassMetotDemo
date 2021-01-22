6612896ee9e0d34e3e5b71a81eba65ba71275c4b
Ferhat KARAGÖZ <pherhad_shyryn@hotmail.com>
2021-01-22 21:43:18 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            
+            Console.WriteLine("Yeni müşteri eklendi : " + musteri.Ad +" "+ musteri.Soyad+ " "+ musteri.Id + " \n");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi : " + musteri.Ad + " " + musteri.Soyad + " " + musteri.Id + "\n");
+
+        }
+
+        public void Listele(Musteri musteri, Musteri musteri1, Musteri musteri2)
+        {
+            Console.WriteLine("Müşteriler listeleniyor : \n "+ musteri.Ad + " " + musteri.Soyad + " " + musteri.Id + " " + musteri.Kimlikno + "\n " + musteri1.Ad + " " + musteri1.Soyad + " " + musteri1.Id + " " + musteri1.Kimlikno +"\n " + musteri2.Ad + " " + musteri2.Soyad + " " + musteri2.Id + " " + musteri2.Kimlikno + "\n");
+        }
+        public void Liste1(Musteri musteri, Musteri musteri1)
+        {
+            Console.WriteLine("Müşteriler listeleniyor : \n " + musteri.Ad + " " + musteri.Soyad + " " + musteri.Id + " " + musteri.Kimlikno + "\n " + musteri1.Ad + " " + musteri1.Soyad + " " + musteri1.Id + " " + musteri1.Kimlikno + "\n ");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Sezen";
+            musteri1.Soyad = "Aksu";
+            musteri1.Id = 1234;
+            musteri1.Kimlikno = 12345678901;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Müslüm";
+            musteri2.Soyad = "Gürses";
+            musteri2.Id = 1235;
+            musteri2.Kimlikno = 12345678902;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Ferdi";
+            musteri3.Soyad = "Tayfur";
+            musteri3.Id = 1236;
+            musteri3.Kimlikno = 12345678903;
+
+            
+
+
+            MusteriManager mm = new MusteriManager();
+
+            mm.Liste1(musteri2, musteri3);
+
+            mm.Ekle(musteri1);
+
+            mm.Listele(musteri1, musteri2, musteri3);
+
+            mm.Sil(musteri3);
+
+           
+
+
+
+        }
+    }
+}
